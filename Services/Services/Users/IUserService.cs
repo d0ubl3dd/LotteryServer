c23c19e1cecd3fe30898354b4e2e89d246a7366b
d0ubl3dd <edgardidier03@gmail.com>
2025-10-11 14:01:53 -0600

    Proyecto Core

--- Services/Services/Users/IUserService.cs
+++ Services/Services/Users/IUserService.cs
@@ -1,12 +1,30 @@
-﻿using System;
+﻿using Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ServiceModel;
+
 
 namespace Services.Services.Users
 {
-    internal interface IUserService
+    [ServiceContract]
+    public interface IUserService
     {
+        [OperationContract]
+        void RegisterUser(User user);
+        [OperationContract]
+        void RegisterGuest(String nickName);
+        [OperationContract]
+        void ChangePassword(String password);
+        [OperationContract]
+        void RecoverPassword(String email);
+        [OperationContract]
+        void UpdateUser(User user);
+        [OperationContract]
+        string GetAvatarPath(int idAvatar);
+        [OperationContract]
+        User GetUser(int idUser);
     }
 }
---
+++ Services/Services/Users/UserService.cs
@@ -0,0 +1,54 @@
+﻿using Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services.Services.Users
+{
+    internal class UserService : IUserService
+    {
+        public void RegisterUser(User user)
+        {
+            var userDAO = new Core.User
+            {
+                id_user = user.id_user,
+                id_avatar = user.id_avatar,
+                nickname = user.nickname,
+                email = user.email,
+                password = user.password,
+                registration_date = user.registration_date,
+                frist_name = user.frist_name,
+                paternal_last_name = user.paternal_last_name,
+                maternal_last_name = user.maternal_last_name,
+                score = user.score
+            };
+            //new UserDAO().AddUser(userDAO);
+        }
+        public void RegisterGuest(string nickName)
+        {
+            throw new NotImplementedException();
+        }
+        public void ChangePassword(string password)
+        {
+            throw new NotImplementedException();
+        }
+        public void RecoverPassword(string email)
+        {
+            throw new NotImplementedException();
+        }
+        public void UpdateUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+        public string GetAvatarPath(int idAvatar)
+        {
+            throw new NotImplementedException();
+        }
+        public User GetUser(int idUser)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
