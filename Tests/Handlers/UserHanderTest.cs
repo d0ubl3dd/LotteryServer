d3ce8b7522e600d7d071bee8407144858e4fa0fb
d0ubl3dd <edgardidier03@gmail.com>
2025-11-29 22:50:28 -0600

    cambios al gitignore

---
+++ Tests/Handlers/UserHanderTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+using Moq;
+using BusinessLogic.Handlers;
+using DataAccess.DAOs;
+using DataAccess;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using Contracts.Faults;
+using BusinessLogic.Logic;
+
+namespace LotteryServer.Tests.Handlers
+{
+    internal class UserHanderTest
+    {
+    }
+}
