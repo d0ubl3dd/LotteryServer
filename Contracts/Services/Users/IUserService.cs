f61be510ac772d3528ebb9d92b18a80edf31c422
CARLOS CASTILLO BARRADAS <zS23014088@estudiantes.uv.mx>
2026-01-13 03:10:39 -0600

    Mensajes ante caída de la base de datos

--- Contracts/Services/Users/IUserService.cs
+++ Contracts/Services/Users/IUserService.cs
@@ -14,9 +14,11 @@ namespace Contracts.Services.Users
         Task<int> RequestUserVerification(UserDto userData);
 
         [OperationContract]
+        [FaultContract(typeof(ServiceFault))]
         Task<int> RegisterUserWithCode(UserDto userData, string code);
 
         [OperationContract]
+        [FaultContract(typeof(ServiceFault))]
         Task<int> RegisterGuest();
 
         [OperationContract]
@@ -27,9 +29,11 @@ namespace Contracts.Services.Users
         Task<bool> ChangePassword(int currentUserId, string newPassword);
 
         [OperationContract]
+        [FaultContract(typeof(ServiceFault))]
         Task<bool> RecoverPassword(string email, string newPassword);
 
         [OperationContract]
+        [FaultContract(typeof(ServiceFault))]
         Task<(bool Success, string Message)> UpdateProfile(int currentUserId, UserDto userData);
 
         [OperationContract]
@@ -37,12 +41,16 @@ namespace Contracts.Services.Users
         Task<FriendDto> FindUserByNickname(string nickname);
 
         [OperationContract]
+        [FaultContract(typeof(ServiceFault))]
         Task<UserDto> GetUserProfile(int currentUserId);
         
         [OperationContract]
         Task<bool> ChangeEmailWithCodeAsync(int currentUserId, string newEmail, string verificationCode);
+
         [OperationContract]
+        [FaultContract(typeof(ServiceFault))]
         Task<bool> RecoverPasswordRequest(string email);
+
         [OperationContract]
         Task<List<LeaderboardPlayerDto>> GetLeaderboard();
     }
